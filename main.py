3a580896037f2fcbaae2b0f7f9df7c1e78a59604
KVN161264 <vadim.centre@mail.ru>
2024-06-23 17:38:25 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+a = (1, 2, 4, 5, 7, 9, 10)
+print(len(a))
+first = 15
+second = 20
+summa = (first + second)
+print(summa)
+diff = (first - second)
+print(diff)
+first = 5
+second = 10
+third = 15
+mean = (first, second, third)
+print(sum(mean)/len(mean))
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(f'{second_string}, {first_string}')
+a = 10
+b = 20
+c = 30
+f = ((a * b) + (a * c))**3/2
+print(f)
